096811d57c6fe5dffea7864494318e4fb6f41763
Nazim N Nazar <112808009+nazimnnazar@users.noreply.github.com>
2023-04-18 23:25:41 +0530

    Update settings.py

--- scuba_diving/settings.py
+++ scuba_diving/settings.py
@@ -12,9 +12,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+z@*t6s+g9se4ga&-b+xzq4l&g01e(2$^(dhl^-pgcp$j@-n!z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
